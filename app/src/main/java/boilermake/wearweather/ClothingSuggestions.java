11f3af8898f0caf1c95b5813b2333425c3f3a429
ChadMcCracken <drewmccutchan@gmail.com>
2017-09-30 18:38:38 -0400

    Clothing Suggestions

--- app/src/main/java/boilermake/wearweather/ClothingSuggestions.java
+++ app/src/main/java/boilermake/wearweather/ClothingSuggestions.java
@@ -1,13 +1,147 @@
 package boilermake.wearweather;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class ClothingSuggestions extends AppCompatActivity {
 
+    String apiText;
+    String result;
+
+    static TextView Descript1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clothing_suggestions);
+        Descript1 = (TextView) findViewById(R.id.Descript);
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        String provider = locationManager.getBestProvider(new Criteria(), false);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Log.d("DEBUG", "Returning - permission");
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+
+//       double lat = location.getLatitude();
+        double lat = 40;
+
+
+//        double lon = location.getLongitude();
+        double lon = -86;
+        WeatherAPI task = new WeatherAPI();
+        String urlStr = "http://api.openweathermap.org/data/2.5/forecast?lat=" + String.valueOf(lat) + "&lon=" + String.valueOf(lon) + "&appid=c5e1f2658752e350a9e5702e334e689d";
+        task.execute(urlStr);
+
+
     }
+
+
+    public class WeatherAPI extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream in = urlConnection.getInputStream();
+
+                final int bufferSize = 1024;
+                final char[] buffer = new char[bufferSize];
+                final StringBuilder out = new StringBuilder();
+                Reader in2 = new InputStreamReader(in, "UTF-8");
+                for (; ; ) {
+                    int rsz = in2.read(buffer, 0, buffer.length);
+                    if (rsz < 0)
+                        break;
+                    out.append(buffer, 0, rsz);
+                }
+                result = out.toString();
+
+                return result;
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+            return result;
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            try {
+
+                JSONObject jsonObject = new JSONObject(result);
+                JSONArray list = jsonObject.getJSONArray("list");
+                ArrayList<String> description = new ArrayList<String>();
+                int count = 3;
+                int i = 1;
+
+                while (count < 27) {
+
+                    JSONObject x = list.getJSONObject(i);
+                    String dt_txt = x.getString("dt_txt");
+                    JSONObject main = x.getJSONObject("weather");
+                    double temp = (main.getDouble("description"));
+                    apiText = Double.toString(temp);
+                    description.add(apiText);
+
+                    count += 3;
+                    i++;
+
+
+                }
+
+                Descript1.setText(description.get(0)+description.get(1)+description.get(2)+description.get(3)+description.get(4)+description.get(5)+description.get(6)+description.get(7));
+
+
+
+            } catch (Exception e) {
+                Log.e("", e.toString());
+            }
+
+            super.onPostExecute(result);
+        }
+    }
+
 }
