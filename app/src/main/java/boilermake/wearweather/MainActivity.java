bbf764ac903f309ef6b58b443c77fbb54ced5fbe
kartikmittal33 <31484338+kartikmittal33@users.noreply.github.com>
2017-09-30 22:57:57 -0400

    REDONE LAYOUT

--- app/src/main/java/boilermake/wearweather/MainActivity.java
+++ app/src/main/java/boilermake/wearweather/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
+        setContentView(R.layout.layout_lay);
 
         // /img.setImageResource(R.drawable.sun);
         hiTempTextView = (TextView) findViewById(R.id.hiTemp);
